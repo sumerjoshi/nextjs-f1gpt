7f9553d3f7135b8def152db1256feaddabd4ecf2
Sumer Joshi <sumerjoshi5@gmail.com>
2025-01-18 17:35:00 -0800

    Code is complete but theres a bug currently

--- app/page.tsx
+++ app/page.tsx
@@ -3,25 +3,53 @@ import Image from "next/image"
 import f1GPTLogo from "./assets/F1-logo.png"
 import { useChat } from "ai/react"
 import { Message } from "ai"
+import Bubble from "./components/bubble"
+import LoadingBubble from "./components/loadingBubble"
+import PromptSuggestionRow from "./components/promptSuggestionsRow"
 
 const Home = () => {
-    const noMessages = true
+
+    const { append, isLoading, messages, input, handleInputChange, handleSubmit } = useChat()
+
+    const noMessages = !messages || messages.length == 0
+
+    const handlePrompt = (promptText: string) => {
+        const msg: Message = {
+            id: crypto.randomUUID(),
+            content: promptText,
+            role: "user"
+
+        }
+        append(msg)
+    }
     return (
         <main>
-            <Image src={f1GPTLogo} width="250" alt="f1GPTLogo"/>
+            <Image src={f1GPTLogo} width="150" alt="f1GPTLogo"/>
+            <section className={noMessages ? "" : "populated"}/>
             <section>
                 {noMessages ? (
                     <>
                         <p className="starter-text">
-
+                            The Ultimate place for Formula F1 superfans! 
+                            Ask F1GPT anything about the fantastic topic of F1 racing.
+                            It will come back with the most up to date answers.
+                            We hope you enjoy!
                         </p>
                         <br/>
+                        <PromptSuggestionRow onPromptClick={handlePrompt}/>
                     </>
 
                 ) : (
-
+                    <>
+                        {messages.map((message, index) => <Bubble key={`message-${index}`} message={message}/>)}
+                        {isLoading && <LoadingBubble/>}
+                    </>
                 )}
             </section>
+            <form onSubmitCapture={handleSubmit}>
+                    <input className="question-box" onChange={handleInputChange} value={input} placeholder="Ask Me Here"/>
+                    <input type="submit"/>
+            </form>
         </main>
     )
 }
